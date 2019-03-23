edaa836ea6affe8aeacc62da6fb614a554f832b5
keepkeyjon <jon@keepkey.com>
2019-03-23 08:37:03 -0600

    WIP: xrp

---
+++ lib/firmware/ripple.c
@@ -0,0 +1,34 @@
+/*
+ * This file is part of the KeepKey project.
+ *
+ * Copyright (C) 2019 ShapeShift
+ *
+ * This library is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "keepkey/firmware/ripple.h"
+
+#include "trezor/crypto/base58.h"
+
+// https://developers.ripple.com/base58-encodings.html
+static const char *ripple_b58digits = "rpshnaf39wBUDNEGHJKLM4PQRST7VWXYZ2bcdeCg65jkm8oFqi1tuvAxyz";
+
+bool ripple_getAddress(const HDNode *node, char address[MAX_ADDR_SIZE])
+{
+    if (!base58_encode_check(node->public_key, 33, HASHER_RIPEMD,
+                             address, MAX_ADDR_SIZE, ripple_b58digits))
+        return false;
+
+    return true;
+}
